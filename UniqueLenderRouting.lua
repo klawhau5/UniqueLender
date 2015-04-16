8827aad20985082c2f25c0a35120742e6037ee1d
Michael McGlinchey <klaw@dhcp-215-74.wireless.bowdoin.edu>
2015-04-16 13:14:24 -0400

    working...

--- UniqueLenderRouting.lua
+++ UniqueLenderRouting.lua
@@ -4,7 +4,13 @@
 
 --Author: Michael McGlinchey, Bowdoin College, 2015
 
-
+local flags
+local Unique_tns
+local Returned_tns
+local RequestFinishedTNs
+local UniqueLenderTNs
+local uniq_len
+local retd_len
 
 function Init()
 	LogDebug("Initializing Unique Lender Routing addon.")
@@ -12,13 +18,25 @@ function Init()
 end
 
 function Begin()
-	local tns = GetTransactions()
-	ProcessDataContexts("TransactionNumber", tns, "RouteFromAwaitingUniqueLender")
+	flags = {}
+	Unique_tns = {}
+	Returned_tns = {}
+	RequestFinishedTNs = {}
+	UniqueLenderTNs = {}
+	uniq_len = 0
+	retd_len = 0
+	Unique_tns = GetUniqueTransactions()
+	Returned_tns = GetReturnedTransactions()
+	if (Unique_tns ~= {} and Returned_tns ~= {}) then
+		local tns = ProcessTransactions()
+		ProcessDataContexts("TransactionNumber", tns, "RouteFromAwaitingUniqueLender")
+	end
 end
 
-function GetTransactions()
+function GetUniqueTransactions()
+	LogDebug("Getting unique lender tns.")
 	local connection = CreateManagedDatabaseConnection()
-	local q = "select t.TransactionNumber "
+	local q = "select TransactionNumber, LendingLibrary, Username "
 	q = q .. "from Transactions t " 
 	q = q .. "where t.TransactionStatus = 'Awaiting Unique Lender'"
 
@@ -27,55 +45,56 @@ function GetTransactions()
 	connection.QueryString = q
 	connection:Connect()
 	local tns = connection:Execute()
-	local UniqueLenderTNs = {}
+
 	for i = 0, tns.Rows.Count - 1 do
 		UniqueLenderTNs[i] = tns.Rows:get_Item(i)
+		uniq_len = uniq_len + 1
 	end
+
+	connection:Dispose()
 
 	table.sort(UniqueLenderTNs, compar)
 
-	connection:Dispose()
+	return UniqueLenderTNs
+end
 
-	--retrieve tns that have just been checked in from customer
+function GetReturnedTransactions()
+	LogDebug("Getting returned recently tns.")
+	local connection = CreateManagedDatabaseConnection()
+	local q = "select TransactionNumber, LendingLibrary, Username "
+	q = q .. "from Transactions t where t.RequestType = 'Loan' " 
+	q = q .. "and t.TransactionStatus = 'Request Finished' "
+	q = q .. "and t.Username != 'Lending' "
+	q = q .. "and DATEDIFF(mi, t.TransactionDate, GETDATE()) < 2"
 
-	q = "select t.TransactionNumber "
-	q = q .. "from Transactions t, History h " 
-	q = q .. "where t.TransactionStatus = 'Request Finished' "
-	q = q .. "and t.TransactionNumber = h.TransactionNumber "
-	q = q .. "and t.TransactionNumber in "
-	q = q .. "(select TransactionNumber from History where TransactionNumber =
-		t.TransactionNumber and Entry = 'Checked Out to Customer' and Datetime = 
-		CONVERT(VARCHAR(12),GETDATE(), 101)"
+	--retrieve tns that have been ret'd today
 
 	connection.QueryString = q
 	connection:Connect()
-	tns = connection:Execute()
-	local RequestFinishedTNs = {}
+	local tns = connection:Execute()
+
+
 	for i = 0, tns.Rows.Count - 1 do
 		RequestFinishedTNs[i] = tns.Rows:get_Item(i)
-		LogDebug("Added from Unique Lender queue: " .. tns.Rows:get_Item(i):get_Item("TransactionNumber"))
+		flags[RequestFinishedTNs[i]:get_Item("TransactionNumber")] = false
+		retd_len = retd_len + 1	
 	end
 
 	connection:Dispose()
 
-	local ReturnTNs = {}
-	--no duplicates
-	local flags = {}
+	return RequestFinishedTNs
+end
 
-	--find tns from recently checked in 
-	--fix the logic of this!!!
-	--or maybe it is fixed...?
-	for i = 0, #RequestFinishedTNs - 1 do
-		for j = 0, #UniqueLenderTNs - 1 do
-			if (UniqueLenderTNs[i]:get_Item("Username") ==
-					RequestFinishedTNs[j].get_Item("Username") and
-					UniqueLenderTNs[i]:get_Item("LendingLibrary") ==
-					RequestFinishedTNs[j]:get_Item("LendingLibrary") and not
-					flags[ReturnTNs[j]:get_Item("TransactionNumber")) then
-				table.insert(ReturnTNs, UniqueLenderTNs[i]:get_Item("TransactionNumber"))
-				flags[ReturnTNs[j]:get_Item("TransactionNumber")] = true
-				i = i + 1
-				j = 0
+function ProcessTransactions()
+	LogDebug("Processing unique/rec.ret tns")
+	
+	local ReturnTNs = {}
+	
+	for i = 0, uniq_len - 1 do
+		for j = 0, retd_len - 1 do
+			if (RequestFinishedTNs[j]:get_Item("Username") == UniqueLenderTNs[i]:get_Item("Username") and RequestFinishedTNs[j]:get_Item("LendingLibrary") == UniqueLenderTNs[i]:get_Item("LendingLibrary") and flags[RequestFinishedTNs[j]:get_Item("TransactionNumber")] ~= true) then
+					table.insert(ReturnTNs, UniqueLenderTNs[i]:get_Item("TransactionNumber"))
+					flags[RequestFinishedTNs[j]:get_Item("TransactionNumber")] = true
 			end
 		end
 	end
@@ -88,18 +107,12 @@ function RouteFromAwaitingUniqueLender()
 	--Article, Book, Book Chapter, Conference, DVD, Document, Thesis,
 	--Microform
 	local docType = GetFieldValue("Transaction", "DocumentType")
-	if docType == "Article" then
-		ExecuteCommand("Route", {tn, "Article Request"})
-	elseif docType == "Book" then
+	if docType == "Book" then
 		ExecuteCommand("Route", {tn, "Book Request"})
 	elseif docType == "Book Chapter" then
 		ExecuteCommand("Route", {tn, "Book Chapter Request"})
-	elseif docType == "Conference" then
-		ExecuteCommand("Route", {tn, "Proceedings Request"})
 	elseif docType == "DVD" then
 		ExecuteCommand("Route", {tn, "Book Request"})
-	elseif docType == "Document" then
-		ExecuteCommand("Route", {tn, "Proceedings Request"})
 	elseif docType == "Thesis" then
 		ExecuteCommand("Route", {tn, "Dissertation Request"})
 	elseif docType == "NPMicro" then
